aab862b449ef749c8f890e91674a6cc4ea484cec
sjy61115 <sjy61115@naver.com>
2024-11-27 16:22:23 +0900

    ts

--- app/src/main/java/com/example/nonograms/MainActivity.java
+++ app/src/main/java/com/example/nonograms/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import android.graphics.Color;
 import android.widget.CompoundButton;
+// test
 
 public class MainActivity extends AppCompatActivity {
     private ToggleButton blackSquareToggle;
